c9d1b3088a4ea17341e917dd8d330fcd8d289b43
mohamed farid <mohamed_hussein5@live.com>
2021-10-31 22:54:24 +0200

    refactor to use DTO from common

--- src/main/java/com/iter/transportation/TransportationApplication.java
+++ src/main/java/com/iter/transportation/TransportationApplication.java
@@ -3,7 +3,7 @@ package com.iter.transportation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.iter.transportation", "com.iter.errorhandling" })
 public class TransportationApplication {
 
 	public static void main(String[] args) {
